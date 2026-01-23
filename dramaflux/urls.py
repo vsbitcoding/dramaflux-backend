e4f63bd11a14f1e58eaabdeeaee989804ba74e24
vsbitcoding <vs.bitcoding@gmail.com>
2026-01-23 10:24:25 +0000

    Add redirect from root URL to Django admin panel

--- dramaflux/urls.py
+++ dramaflux/urls.py
@@ -3,8 +3,10 @@ URL configuration for dramaflux project.
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/', include('dramas.urls')),
 ]
